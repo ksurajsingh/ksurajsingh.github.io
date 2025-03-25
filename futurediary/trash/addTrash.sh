f3bf41795d6374fa5459dc59b6f29af34478744b
Surajk013 <surajsinghk013@gmail.com>
2025-03-25 06:00:14 +0530

    clickable-cards+proper-preview[TrashBlogs]

--- futurediary/trash/addTrash.sh
+++ futurediary/trash/addTrash.sh
@@ -27,6 +27,6 @@ for file in *.html; do
     Preview=$(sed -zn "s:.*<p id=\"blog\">\(.*\)</p>.*:\1:p" $file | head -n 2 | sed -n "s/<br>//gp" )
     time=$(python estTime.py $file)
     date=$file
-    echo -e "<br><div class=\"blog-card\"><hr><div class=\"blog-title\"><a href=\"$file\">$Heading</a><span class=\"date\">$date</span></div><br>\n<span class=\"tag philosophy\">$Category</span><span class=\"time\">$time</span><br>\n$Preview<br><hr></div>" >> trash.html 
+    echo -e "<br><a href=\"$file\"><div class=\"blog-card\"><hr><div class=\"blog-title\">$Heading<span class=\"date\">$date</span></div>\n<span class=\"tag philosophy\">$Category</span><span class=\"time\">$time</span><br>\n$Preview<br><hr></div></a>" >> trash.html 
   fi
 done
