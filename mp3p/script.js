4aa8849c3d0caea96eaaf50933cec64e83eedd58
ksurajsingh <surajsinghk013@gmail.com>
2025-08-21 16:33:04 +0530

    feat(mp3p):able to fetch songs
    in the form of json response right now

--- mp3p/script.js
+++ mp3p/script.js
@@ -1,6 +1,30 @@
 console.log("JS Loaded 😋");
-// const dir="/mnt/KSS/Media/songs/"
-const dir="http://localhost:8055/."
-const rawSongs=fetch(dir);
-console.log("fetched huh");
-console.log(rawSongs);
+
+// test connection to server
+async function testConnectionToServer(){
+  const dir="http://localhost:8055/.";
+  const respraw=await fetch(dir);
+  const resp=await respraw.json()
+  console.log(resp.message);
+}
+
+
+// fetch songs from server
+async function getSongs(){
+
+  // const dir="/mnt/KSS/Media/songs/"
+  const dir="http://localhost:8055/songs";
+  const respraw=await fetch(dir);
+  const resp=await respraw.json();
+  console.log(resp.message);
+  return resp;
+
+}
+
+// initialise processes for the client
+function init(){
+testConnectionToServer();
+const songsList=getSongs()
+}
+
+init();
