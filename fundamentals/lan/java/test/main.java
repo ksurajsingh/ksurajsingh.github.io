359954633057c7d999c5715aee62fe464dada42e
ksurajsingh <surajsinghk013@gmail.com>
2025-08-25 21:38:15 +0530

    feat(lan):add java

---
+++ fundamentals/lan/java/test/main.java
@@ -0,0 +1,9 @@
+package test;
+
+public class main{
+  public static void main(String args[]){
+    student suraj=new student();
+    suraj.name="suraj singh";
+    System.out.println(suraj.name);
+  }
+}
